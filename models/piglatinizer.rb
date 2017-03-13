455ad71771872f80aae66b84b26e9f05ea0ba166
github username <github email address>
2017-03-12 22:18:18 -0500

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,14 @@
+class PigLatinizer
+
+  attr_accessor :text
+
+  def piglatinize(word)
+    prefix = word.slice!(/^[^AEIOUaeiou]*/)
+    word << prefix << (prefix.length > 0 ? "ay" : "way")
+  end
+
+  def to_pig_latin(text)
+    text.split(' ').map{|w| piglatinize(w) }.join(' ')
+  end
+
+end
