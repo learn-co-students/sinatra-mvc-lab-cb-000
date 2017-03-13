f03e255b8db47e14d447e303d9aa06418468c491
github username <github email address>
2017-03-12 22:49:15 -0500

    Done.

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   post "/piglatinize" do
-    @converted = PigLatinizer.new.to_pig_latin(params[:user_phrase])
+    @pig_latin = PigLatinizer.new.to_pig_latin(params[:user_phrase])
     erb :piglatinize
   end
 
