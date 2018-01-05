159030d138e8cf904b7ebb7d671a601b79ae1e8b
Michael Anderson <michanders@msn.com>
2018-01-05 21:54:21 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    resp = Rack::Response.new
+    resp.status = 200
+
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    resp = Rack::Response.new
+    resp.status = 200
+    phrase = params[:user_phrase]
+    pig = PigLatinizer.new
+    @pig = pig.to_pig_latin(phrase)
+
+    erb :new_views
+  end
 end
