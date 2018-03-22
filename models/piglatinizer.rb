ddd397727003d160806c4f88992f7bf99e0143d8
adam n <adam.nawroc@gmail.com>
2018-03-22 16:42:35 +0000

    progress

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,30 +2,38 @@ class PigLatinizer
 
   def piglatinize(word)
     # i can do this
-    vowels = ['a', 'e', 'i', 'o', 'u']
 
-    # if word beings with vowel
-    if vowels.include? word[0].downcase
-      word + 'way'
-      # feel like this isnt the tightest. ill go w/ it
-    # if second letter is a vowel
-    elsif vowels.include? word[1].downcase
-      word[1..-1] + word[0] + 'ay'
+    def singleWord(word)
+	    vowels = ['a', 'e', 'i', 'o', 'u']
+      if vowels.include? word[0].downcase
+        word + 'way'
+        # feel like this isnt the tightest. ill go w/ it
+      # if second letter is a vowel
+      elsif vowels.include? word[1].downcase
+        word[1..-1] + word[0] + 'ay'
+      else
+
+        # im sure this could be tighter
+        vowelSounds = []
+        word.split('').each do |i|
+          break if vowels.include? i
+
+          vowelSounds << i
+        end
+        word[vowelSounds.size..-1]+vowelSounds.join('')+'ay'
+    end
+    end
+
+    if !word.include? ' '
+      singleWord(word)
     else
+      ans = word.split(' ').collect do |i|
+        singleWord(i)
 
-      # im sure this could be tighter
-      vowelSounds = []
-      word.split('').each do |i|
-        break if vowels.include? i
-
-        vowelSounds << i
       end
 
-      # word.slice(0, vowelSounds.size)+ word[vowelSounds.size..-1]+vowelSounds.join('')
-      word[vowelSounds.size..-1]+vowelSounds.join('')+'ay'
-
-  end
-
+      ans.join(' ')
+    end
   end
 
 end
