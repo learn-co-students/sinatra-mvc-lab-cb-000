4ec050db6d2a901dd347c15e6a1641a75fafbf05
Liz Jenerson <liz.jenerson@gmail.com>
2018-05-10 22:27:31 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,18 +2,27 @@ class PigLatinizer
 
   attr_accessor :word
 
-  def piglatinize(word)
-    case word
-    when /^[aeiou]/
-        word + "way"
-
-      else
-
-        after_vowel = word.slice(/[aeiou]\w+/)
-        before_vowel = word.chomp(after_vowel)
-        after_vowel + before_vowel + "ay"
-      end
-
+  def piglatinize(phrase)
+    pig_sentence(phrase)
   end
 
+  def pig_word(word)
+    if word =~ /^[AEIOU]|^[aeiou]/
+      word + "way"
+    else
+      after_vowel = word.slice(/[aeiou]\w+/)
+      before_vowel = word.chomp(after_vowel)
+      after_vowel + before_vowel + "ay"
+    end
+  end
+
+  def pig_sentence(phrase)
+    phrase_to_array = phrase.scan(/\w+/)
+
+    sentence = phrase_to_array.map do |p|
+      pig_word(p)
+    end
+
+    sentence.join(" ")
+  end
 end
