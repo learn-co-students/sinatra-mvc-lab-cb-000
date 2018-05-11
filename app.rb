3bfc586b51fa81398cd92c2bd445694c622eee84
Liz Jenerson <liz.jenerson@gmail.com>
2018-05-11 09:25:22 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/" do
+    erb :index
+  end
+
+  post "/piglatinize" do
+
+    @pig_latinizer = PigLatinizer.new
+
+    erb :piglatinize
+
+  end
+
+
 end
