d8a3017e97d9c2566d664f4f4ce36346399cbf98
Efua Mensah-Brown <emensahb@efbelles-laptop.lippuner.ca>
2019-10-21 13:44:58 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
+#require_relative 'models/piglatinzer.rb'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+
+  end
+
+
+  post '/piglatinize' do
+    p = PigLatinizer.new
+    @user_phrase = p.piglatinize(params[:user_phrase].to_s)
+    erb :piglatinize
+  end
+
+
+
+
 end
